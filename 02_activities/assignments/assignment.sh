7f3a6fdade8d2100bac386ac07f0dbd4edbcaaa6
ldanica <danicaleung24@gmail.com>
2024-08-23 00:20:18 -0400

    Updated answer #3

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -14,7 +14,11 @@ mkdir dir1 dir2 dir3 dir4 dir5
 ls
 
 # 3. Create 5 text files in dir2 named file1.txt, file2.txt, file3.txt, file4.txt, and file5.txt
-touch file1.txt file2.txt file3.txt file4.txt file5.txt dir2/
+touch dir2/file1.txt
+touch dir2/file2.txt
+touch dir2/file3.txt
+touch dir2/file4.txt
+touch dir2/file5.txt
 
 # 4. Append the words "Hello world" to dir2/file3.txt
 echo 'Hello world' > dir2/file3.txt
